5170afd5d1dc2e3485109401a9bc0374e8f31b37
NilsHeil <56686609+NilsHeil@users.noreply.github.com>
2023-07-28 10:50:13 -0600

    progress

--- task1_nils.py
+++ task1_nils.py
@@ -2,23 +2,7 @@ import warnings
 import smatch_modified
 import pandas as pd
 
-def calc_with_warnings(true_amr, pred_amr):
-    try:
-        # redirect warnings 
-        with warnings.catch_warnings(record=True) as w:
-            best_match_num, test_triple_num, gold_triple_num = smatch_modified.get_amr_match(true_amr, pred_amr)
-        
-            if w:
-                error_message = str(w[-1].message)
-            else:
-                error_message = None
-                
-            return best_match_num, test_triple_num, gold_triple_num, error_message
 
-    except Exception as e:
-        # if exception is raised, capture error msg
-        error_message = str(e)
-        return None, None, None, error_message
 
 
 df = pd.read_csv("bio_amr_gpt4_invalid.csv")
@@ -27,18 +11,14 @@ data = df.to_dict(orient='records')
 
 true_amr = data[1]['gold_amr']
 pred_amr = data[1]['gpt4_0613_amr']
-id = data[0]['id']
 
 
-best_match, test_triple_num, gold_triple_num, error_message = calc_with_warnings(true_amr, pred_amr)
+
+best_match_num, test_triple_num, gold_triple_num, error_message = smatch_modified.get_amr_match(true_amr, pred_amr)
 
 if error_message is not None:
-    print("id:", id)
     print("Error:", error_message)
-else: 
-    print("best match:", best_match)
-    print("test_triple_num:", test_triple_num)
-    print("gold_triple_num:", gold_triple_num)
+
 
 
 
